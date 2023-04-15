8a9011954252d251d53607a3c16d4d967b30a4c5
Digitall228 <dia_odessa5@ukr.net>
2023-04-15 15:01:12 +0100

    Add project files.

---
+++ AddFriend(redBubble)/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace AddFriend_redBubble_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.LogBox = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(93, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(259, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Start acc";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Login";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(93, 32);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(259, 20);
+            this.textBox2.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(93, 59);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(259, 20);
+            this.textBox3.TabIndex = 0;
+            this.textBox3.Text = "https://www.redbubble.com/people/thepapercrane/following?ref=profile-header";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(12, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(277, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // LogBox
+            // 
+            this.LogBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.LogBox.Location = new System.Drawing.Point(12, 124);
+            this.LogBox.Multiline = true;
+            this.LogBox.Name = "LogBox";
+            this.LogBox.Size = new System.Drawing.Size(340, 84);
+            this.LogBox.TabIndex = 4;
+            this.LogBox.Text = "Adder in redBubble";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(108, 95);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "StartBrowser";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 220);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.LogBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "AddFriend";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox LogBox;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ AddFriend(redBubble)/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AddFriend_redBubble_
+{
+    public partial class Form1 : Form
+    {
+        IWebDriver Browser;
+        Thread mainThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            LogAdd("Initialization..");
+            mainThread = new Thread(Main);
+            mainThread.Start();
+        }
+
+        void Main()
+        {//a[rel='next']
+            try
+            {
+                string startUrl = textBox3.Text;
+                List<List<int>> followersCount = new List<List<int>>();
+                List<List<int>> notCheck = new List<List<int>>();
+                List<List<string>> urls = new List<List<string>>();
+                List<int> not = new List<int>();
+                List<string> notU = new List<string>();
+                int theBest = 0;
+                int now = 0;
+                LogAdd("Starting..");
+
+                Browser.Navigate().GoToUrl(textBox3.Text);
+
+                while (true)
+                {//
+                    Browser.Navigate().GoToUrl(startUrl);
+
+
+
+                    List<IWebElement> PageChanger = Browser.FindElements(By.CssSelector("a[rel='next']")).ToList();
+                    for (int hj = 0; hj <= 0;)
+                    {
+                        PageChanger = Browser.FindElements(By.CssSelector("a[rel='next']")).ToList();
+                        if (PageChanger.Count < 0) hj++;
+
+                        List<IWebElement> Accs = Browser.FindElements(By.CssSelector(".oldgrid li a[title]")).ToList();
+                        for (int y = 0; y < Accs.Count; y++)
+                        {
+                            Accs = Browser.FindElements(By.CssSelector(".oldgrid li a[title]")).ToList();
+                            Accs[y].Click();
+
+                            List<IWebElement> Check = Browser.FindElements(By.CssSelector(".no-results.group")).ToList();
+                            if (Check.Count == 0)
+                            {//
+                                
+
+                                IWebElement Followers = Browser.FindElement(By.CssSelector("a[data-reactid='20']"));
+                                not.Add(Convert.ToInt32(Regex.Replace(Followers.Text, "([^0-9]|\\$|,)", "")));
+                                notU.Add(Browser.Url);
+                                List<IWebElement> Follow = Browser.FindElements(By.CssSelector("a[title='Start Following']")).ToList();
+                                if (Follow.Count > 0)
+                                {
+                                    List<IWebElement> WindowClose = Browser.FindElements(By.CssSelector(".sailthru-overlay-close")).ToList();
+                                    if (WindowClose.Count > 0)
+                                        WindowClose[0].Click();
+                                    Follow[0].Click();
+                                }
+                                
+                                LogAdd((y + 1) + " account is valid.");
+                            }
+                            else { LogAdd((y + 1) + " account isn't valid."); }
+                            Browser.Navigate().Back();
+                        }
+                        PageChanger = Browser.FindElements(By.CssSelector("a[rel='next']")).ToList();
+                        if (hj <= 0)
+                            PageChanger[1].Click();
+
+                    }
+                    int jh = 0;
+                    followersCount.Add(not);
+                    urls.Add(notU);
+                    for (int o = 1; o < followersCount.Count; o++)
+                    {
+                        if (followersCount[now][o] > followersCount[now][o - 1])
+                        {
+                            for (int q = 0; q < notCheck.Count; q++)
+                            {
+                                if (followersCount[now][o] == notCheck[now][q])
+                                {
+                                    jh++;
+                                }
+                            }
+                            if (jh == 0)
+                            {
+                                theBest = o;
+                            }
+                            else { jh = 0; }
+                        }
+                    }
+
+                    startUrl = urls[now][theBest];
+                    followersCount[now].RemoveAt(theBest);
+                    urls[now].RemoveAt(theBest);
+                    now++;
+                }
+            }
+            catch
+            { }
+        }
+
+        void Login()
+        {//
+            IWebElement LoginSend = Browser.FindElement(By.CssSelector("#ReduxFormInput1"));
+            LoginSend.SendKeys(textBox1.Text);
+            IWebElement PassSend = Browser.FindElement(By.CssSelector("#ReduxFormInput2"));
+            PassSend.SendKeys(textBox2.Text);
+            IWebElement Submit = Browser.FindElement(By.CssSelector("span button[type='submit']"));
+            Submit.Click();
+            LogAdd("Logged in, loading...");
+
+        }
+
+        void StartBrowser()
+        {
+            var driverService = ChromeDriverService.CreateDefaultService();
+            ChromeOptions options = new ChromeOptions();
+
+            //driverService.HideCommandPromptWindow = true;
+            //options.AddArgument("--headless");
+
+            //options.AddExtension("3.22.1_0.crx");
+            Browser = new OpenQA.Selenium.Chrome.ChromeDriver(driverService, options);
+            Browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            //Browser.Manage().Window.Maximize();
+            Browser.Navigate().GoToUrl("https://www.redbubble.com/auth/login");
+            LogAdd("Loading....");
+        }
+
+        void LogAdd(string text)
+        {
+            /*string mes = DateTime.Now + " " + text + Environment.NewLine;
+            LogBox.AppendText(mes);*/
+            LogBox.AppendText(DateTime.Now + " " + text + Environment.NewLine);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mainThread.Abort();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StartBrowser();
+            Login();
+        }
+    }
+}
